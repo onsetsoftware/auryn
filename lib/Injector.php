36d19ed54f5fb1a4a3075f8a297ff04446b30ddf
Félix Gagnon-Grenier <fractalesque@gmail.com>
2025-03-02 16:52:12 -0500

    Refactor the tests to pass github actions pipeline (#202)

--- lib/Injector.php
+++ lib/Injector.php
@@ -4,40 +4,40 @@ namespace Auryn;
 
 class Injector
 {
-    const A_RAW = ':';
-    const A_DELEGATE = '+';
-    const A_DEFINE = '@';
-    const I_BINDINGS = 1;
-    const I_DELEGATES = 2;
-    const I_PREPARES = 4;
-    const I_ALIASES = 8;
-    const I_SHARES = 16;
-    const I_ALL = 31;
+    public const A_RAW = ':';
+    public const A_DELEGATE = '+';
+    public const A_DEFINE = '@';
+    public const I_BINDINGS = 1;
+    public const I_DELEGATES = 2;
+    public const I_PREPARES = 4;
+    public const I_ALIASES = 8;
+    public const I_SHARES = 16;
+    public const I_ALL = 31;
 
-    const E_NON_EMPTY_STRING_ALIAS = 1;
-    const M_NON_EMPTY_STRING_ALIAS = "Invalid alias: non-empty string required at arguments 1 and 2";
-    const E_SHARED_CANNOT_ALIAS = 2;
-    const M_SHARED_CANNOT_ALIAS = "Cannot alias class %s to %s because it is currently shared";
-    const E_SHARE_ARGUMENT = 3;
-    const M_SHARE_ARGUMENT = "%s::share() requires a string class name or object instance at Argument 1; %s specified";
-    const E_ALIASED_CANNOT_SHARE = 4;
-    const M_ALIASED_CANNOT_SHARE = "Cannot share class %s because it is currently aliased to %s";
-    const E_INVOKABLE = 5;
-    const M_INVOKABLE = "Invalid invokable: callable or provisional string required";
-    const E_NON_PUBLIC_CONSTRUCTOR = 6;
-    const M_NON_PUBLIC_CONSTRUCTOR = "Cannot instantiate protected/private constructor in class %s";
-    const E_NEEDS_DEFINITION = 7;
-    const M_NEEDS_DEFINITION = "Injection definition required for %s %s";
-    const E_MAKE_FAILURE = 8;
-    const M_MAKE_FAILURE = "Could not make %s: %s";
-    const E_UNDEFINED_PARAM = 9;
-    const M_UNDEFINED_PARAM = "No definition available to provision typeless parameter \$%s at position %d in %s()%s";
-    const E_DELEGATE_ARGUMENT = 10;
-    const M_DELEGATE_ARGUMENT = "%s::delegate expects a valid callable or executable class::method string at Argument 2%s";
-    const E_CYCLIC_DEPENDENCY = 11;
-    const M_CYCLIC_DEPENDENCY = "Detected a cyclic dependency while provisioning %s";
-    const E_MAKING_FAILED = 12;
-    const M_MAKING_FAILED = "Making %s did not result in an object, instead result is of type '%s'";
+    public const E_NON_EMPTY_STRING_ALIAS = 1;
+    public const M_NON_EMPTY_STRING_ALIAS = "Invalid alias: non-empty string required at arguments 1 and 2";
+    public const E_SHARED_CANNOT_ALIAS = 2;
+    public const M_SHARED_CANNOT_ALIAS = "Cannot alias class %s to %s because it is currently shared";
+    public const E_SHARE_ARGUMENT = 3;
+    public const M_SHARE_ARGUMENT = "%s::share() requires a string class name or object instance at Argument 1; %s specified";
+    public const E_ALIASED_CANNOT_SHARE = 4;
+    public const M_ALIASED_CANNOT_SHARE = "Cannot share class %s because it is currently aliased to %s";
+    public const E_INVOKABLE = 5;
+    public const M_INVOKABLE = "Invalid invokable: callable or provisional string required";
+    public const E_NON_PUBLIC_CONSTRUCTOR = 6;
+    public const M_NON_PUBLIC_CONSTRUCTOR = "Cannot instantiate protected/private constructor in class %s";
+    public const E_NEEDS_DEFINITION = 7;
+    public const M_NEEDS_DEFINITION = "Injection definition required for %s %s";
+    public const E_MAKE_FAILURE = 8;
+    public const M_MAKE_FAILURE = "Could not make %s: %s";
+    public const E_UNDEFINED_PARAM = 9;
+    public const M_UNDEFINED_PARAM = "No definition available to provision typeless parameter \$%s at position %d in %s()%s";
+    public const E_DELEGATE_ARGUMENT = 10;
+    public const M_DELEGATE_ARGUMENT = "%s::delegate expects a valid callable or executable class::method string at Argument 2%s";
+    public const E_CYCLIC_DEPENDENCY = 11;
+    public const M_CYCLIC_DEPENDENCY = "Detected a cyclic dependency while provisioning %s";
+    public const E_MAKING_FAILED = 12;
+    public const M_MAKING_FAILED = "Making %s did not result in an object, instead result is of type '%s'";
 
     private $reflector;
     private $classDefinitions = array();
@@ -50,7 +50,7 @@ class Injector
 
     public function __construct(Reflector $reflector = null)
     {
-        $this->reflector = $reflector ?: new CachingReflector;
+        $this->reflector = $reflector ?: new CachingReflector();
     }
 
     public function __clone()
@@ -174,7 +174,7 @@ class Injector
     private function shareClass($nameOrInstance)
     {
         [, $normalizedName] = $this->resolveAlias($nameOrInstance);
-        $this->shares[$normalizedName] = isset($this->shares[$normalizedName])? $this->shares[$normalizedName]: null;
+        $this->shares[$normalizedName] = isset($this->shares[$normalizedName]) ? $this->shares[$normalizedName] : null;
     }
 
     private function resolveAlias($name)
@@ -378,12 +378,7 @@ class Injector
             }
 
             unset($this->inProgressMakes[$normalizedClass]);
-        }
-        catch (\Throwable $exception) {
-            unset($this->inProgressMakes[$normalizedClass]);
-            throw $exception;
-        }
-        catch (\Exception $exception) {
+        } catch (\Throwable $exception) {
             unset($this->inProgressMakes[$normalizedClass]);
             throw $exception;
         }
@@ -413,7 +408,7 @@ class Injector
                     self::E_NON_PUBLIC_CONSTRUCTOR
                 );
             } elseif ($ctorParams = $this->reflector->getCtorParams($className)) {
-                $aggregatedArgs = isset($this->classDefinitions[$normalizedClass])? array_replace($this->classDefinitions[$normalizedClass], $args): $args;
+                $aggregatedArgs = isset($this->classDefinitions[$normalizedClass]) ? array_replace($this->classDefinitions[$normalizedClass], $args) : $args;
                 $obj = $this->reflector
                   ->getClass($className)
                   ->newInstanceArgs($this->provisionFuncArgs($ctor, $aggregatedArgs, $ctorParams, $className));
@@ -445,7 +440,7 @@ class Injector
             );
         }
 
-        return new $className;
+        return new $className();
     }
 
     /**
@@ -476,10 +471,10 @@ class Injector
             } elseif (($prefix = self::A_DEFINE . $paramName) && isset($definition[$prefix])) {
                 // interpret the param as a class definition
                 $arg = $this->buildArgFromParamDefineArr($definition[$prefix]);
-            /**
-             * This will look for a share, then a delegate, and then attempt an autoload if none were found.
-             * Failing all that it will use the default value...
-             */
+                /**
+                 * This will look for a share, then a delegate, and then attempt an autoload if none were found.
+                 * Failing all that it will use the default value...
+                 */
             } elseif (!$arg = $this->buildArgFromTypeHint($reflFunc, $reflParam)) {
                 $arg = $this->buildArgFromReflParam($reflParam, $className);
 
@@ -544,9 +539,8 @@ class Injector
                 array_key_exists($normalizedName, $this->shares) ||
                 class_exists($typeHint)) {
                 $obj = $this->make($typeHint);
-            }
-            else {
-              $obj = $reflParam->getDefaultValue();
+            } else {
+                $obj = $reflParam->getDefaultValue();
             }
         } else {
             $obj = $this->make($typeHint);
@@ -603,7 +597,7 @@ class Injector
             }
         }
 
-        $interfaces = is_object($obj)? @class_implements($obj): false;
+        $interfaces = is_object($obj) ? @class_implements($obj) : false;
 
         if ($interfaces === false) {
             throw new InjectionException(
@@ -623,7 +617,7 @@ class Injector
 
         $interfaces = array_flip(array_map(array($this, 'normalizeName'), $interfaces));
         $prepares = array_intersect_key($this->prepares, $interfaces);
-        foreach ($prepares as $interfaceName => $prepare) {
+        foreach ($prepares as $prepare) {
             $executable = $this->buildExecutable($prepare);
             $result = $executable($obj, $this);
             if ($result instanceof $normalizedClass) {
